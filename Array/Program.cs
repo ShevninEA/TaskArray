d4e8a08241263b284acfbc75490c2509d63161ea
user-ISK26 <user-ISK26@NOTEBOOK-ISK5>
2023-02-07 18:40:36 +0300

    Добавьте файлы проекта.

---
+++ Array/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace TaskArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = { "Hello", "my", "int", "log", "Int32", "var", "str"};
+
+            string[] myArray2 = new string[myArray.Length];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i].Length <= 3)
+                {
+                    myArray2[i] = myArray[i];
+                }
+                continue;
+            }
+
+            Array.Sort(myArray2);
+
+            for (int i = 0; i < myArray2.Length; i++)
+            {
+                Console.Write(myArray2[i] + " ");
+            }
+        }
+    }
+}
